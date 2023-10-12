9875e6b5face8eaba80d30e74a9420f279ff43ea
Stephane Mirada <stephane.miranda@liferay.com>
2023-10-12 14:30:33 -0300

    Refactory and add scenarios

--- src/models/ice_cream_stand.py
+++ src/models/ice_cream_stand.py
@@ -4,40 +4,54 @@ from src.models.restaurant import Restaurant
 class IceCreamStand(Restaurant):
     """Um tipo especializado de restaurante."""
 
-    def __init__(self, restaurant_name, cuisine_type, flavors_list):
+    def __init__(self, restaurant_name, cuisine_type, flavors_list, open=True):
         """
         Inicialize os atributos da classe pai.
         Em seguida, inicialize os atributos específicos de uma sorveteria.
         """
         super().__init__(restaurant_name, cuisine_type)
+        self.open = open
         self.flavors = flavors_list
 
+    # Refactory:
+    # 1. Uma lista vazia chamada available_flavors foi criada para armazenar os sabores disponíveis. Isso permite que a função acumule os sabores disponíveis na lista.
+    # 2. Em vez de usar print para mostrar os sabores na saída, a função agora usa append para adicionar cada sabor à lista available_flavors.
+    # 3. A função retorna a lista available_flavors quando termina de percorrer os sabores.
+    # 4. Se a lista de sabores estiver vazia, a função retorna uma lista vazia, indicando que não há sabores disponíveis.
     def flavors_available(self):
         """Percorra a lista de sabores disponíveis e imprima."""
         if self.flavors:
+            available_flavors = []
             print("\nNo momento temos os seguintes sabores de sorvete disponíveis:")
             for flavor in self.flavors:
+                available_flavors.append(flavor)
                 print(f"\t-{flavor}")
+            return available_flavors
         else:
             print("Estamos sem estoque atualmente!")
+            return []
 
+    # Refactory:
+    # 1, Adicionado o "return" dentro da método de verificação.
     def find_flavor(self, flavor):
-        """Verifica se o sabor informado está disponível."""
+        """Verifica se o sabor informado está disponível na lista de sabores."""
         if self.flavors:
             if flavor in self.flavors:
-                print(f"Temos no momento {self.flavors}!")
+                return f"O sabor '{flavor}' está disponível no momento."
             else:
-                print(f"Não temos no momento {self.flavors}!")
+                return f"O sabor '{flavor}' não está disponível no momento."
         else:
-            print("Estamos sem estoque atualmente!")
+            return "Estamos sem estoque atualmente."
 
+    # Refactory:
+    # 1. Adicionado o "return" dentro da método de verificação.
     def add_flavor(self, flavor):
-        """Add o sabor informado ao estoque."""
-        if self.flavors:
+        """Adiciona o sabor informado ao estoque."""
+        if self.open:
             if flavor in self.flavors:
-                print("\nSabor já disponivel!")
+                return f"O sabor '{flavor}' já está disponível no estoque."
             else:
                 self.flavors.append(flavor)
-                print(f"{flavor} adicionado ao estoque!")
+                return f"O sabor '{flavor}' foi adicionado ao estoque."
         else:
-            print("Estamos sem estoque atualmente!")
+            return "A sorveteria está fechada, não é possível adicionar sabores."
--- test/models/test_ice_cream_stand.py
+++ test/models/test_ice_cream_stand.py
@@ -1,10 +1,136 @@
-class TestIceCreamStand:
+import pytest
+import sys
+from io import StringIO
 
-    def test_flavors_available(self):
-        assert False
 
-    def test_find_flavor(self):
-        assert False
+from src.models.ice_cream_stand import IceCreamStand
 
-    def test_add_flavor(self):
-        assert False
+
+class TestIceCreamStand():
+
+    # Teste com uma lista não vazia
+    def test_flavors_available_non_empty(self):
+        ##Setup
+        list_flavors_available = ["Morango", "Chocolate", "Baunilha", "Pistache"]
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_available)
+
+        ##Chamada
+        result = iceCream.flavors_available()
+
+        ##Avaliação
+        assert result == list_flavors_available
+
+    # Teste com uma lista vazia
+    def test_flavors_available_empty(self):
+        ##Setup
+        list_flavors_empty = []
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_empty)
+
+        ##Chamada
+        result = iceCream.flavors_available()
+
+        ##Avaliação
+        assert result == []
+
+    # Teste com saída impressa
+    @pytest.mark.usefixtures('capsys')
+    def test_flavors_available_output(capsys):
+        ##Setup
+        list_flavors_available = ["Morango", "Chocolate", "Baunilha", "Pistache"]
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_available)
+
+        # Redirecione a saída padrão para um buffer
+        original_stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Chame o método flavors_available
+        iceCream.flavors_available()
+
+        # Recupere a saída capturada
+        output = sys.stdout.getvalue()
+
+        # Restaure a saída padrão original
+        sys.stdout = original_stdout
+
+        ##Avaliação
+        assert "No momento temos os seguintes sabores de sorvete disponíveis:" in output
+        assert "Estamos sem estoque atualmente!" not in output
+
+    # Teste com lista de sabores None
+    def test_flavors_available_none(self):
+        ##Setup
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", None)
+
+        ##Chamada
+        result = iceCream.flavors_available()
+
+        ##Avaliação
+        assert result == []
+
+    # Teste para sabor disponível
+    def test_find_flavor_available(self):
+        ##Setup
+        list_flavors_available = ["Morango", "Chocolate", "Baunilha", "Pistache"]
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_available)
+
+        ##Chamada
+        result = iceCream.find_flavor("Morango")
+
+        ##Avaliação
+        assert result == "O sabor 'Morango' está disponível no momento."
+
+    # Teste para sabor não disponível
+    def test_find_flavor_not_available(self):
+        ##Setup
+        list_flavors_available = ["Morango", "Chocolate", "Baunilha", "Pistache"]
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_available)
+
+        ##Chamada
+        result = iceCream.find_flavor("Caramelo")
+
+        ##Avaliação
+        assert result == "O sabor 'Caramelo' não está disponível no momento."
+
+    def test_find_flavor_empty_list(self):
+        ##Setup
+        list_flavors_empty = []
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_empty)
+
+        ##Chamada
+        result = iceCream.find_flavor('Pistache')
+
+        ##Avaliação
+        assert result == "Estamos sem estoque atualmente."
+
+    # Teste para adicionar sabor que já está disponível
+    def test_add_flavor_already_available(self):
+        ##Setup
+        list_flavors_available = ["Morango", "Chocolate", "Baunilha", "Pistache"]
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_available)
+
+        ##Chamada
+        result = iceCream.add_flavor("Baunilha")
+
+        ##Avaliação
+        assert result == "O sabor 'Baunilha' já está disponível no estoque."
+
+    # Teste para adicionar sabor quando a sorveteria está fechada
+    def test_add_flavor_when_closed(self):
+        ##Setup
+        list_flavors_available = ["Morango", "Chocolate", "Baunilha", "Pistache"]
+
+        ##Chamada
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_available, open=False)
+        result = iceCream.add_flavor("Caramelo")
+
+        ##Avaliação
+        assert result == "A sorveteria está fechada, não é possível adicionar sabores."
+
+    # Teste para adicionar sabor quando a lista de sabores está vazia
+    def test_add_flavor_to_empty_list(self):
+        ##Setup
+        list_flavors_empty = []
+        iceCream = IceCreamStand("Minha Sorveteria", "Sorvetes", list_flavors_empty)
+
+        result = iceCream.add_flavor("Caramelo")
+        assert result == "O sabor 'Caramelo' foi adicionado ao estoque."
